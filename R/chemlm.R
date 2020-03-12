8e3d8dad0697dfb557311b8762c44f8c3af6159e
kralljr <jenna.krall@gmail.com>
2020-03-12 14:00:30 -0400

    Fix chem name for unadjusted model

--- R/chemlm.R
+++ R/chemlm.R
@@ -53,7 +53,7 @@ chemlm.default <- function(data, outcome, chem, value = "value", adjust = NULL, 
 
   # if confounders included, run null model
   if(!is.null(confound)) {
-    nest_vars <- colnames(data)[colnames(data) != "chem"]
+    nest_vars <- colnames(data)[colnames(data) != chem]
     dataU <- nest(data, chemdat = one_of(nest_vars))  %>%
       # run regression, getting relevant output
       mutate(fit = map(chemdat, ~ innerchemlm(data = ., outcome = outcome, value = value,
@@ -116,7 +116,9 @@ innerchemlm <- function(data, outcome, value = "value", confound = NULL, family 
   # run model, get 95% CI
   glm1 <- glm(eval(eqn), family = family, data = data)
   output <- summary(glm1)$coef
-  output <- data.frame(names = rownames(output), output, suppressMessages(confint(glm1)))
+  output <- try(data.frame(names = rownames(output), output, suppressMessages(confint(glm1))))
+
+  if(class(output) == "try-error") {browser()}
 
   colnames(output) <- c("names", "est", "SE", "z", "pvalue", "lb", "ub")
   #return all
